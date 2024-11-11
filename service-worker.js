5f764acefe9a7b5f9efa98aa76b5bb431193a126
chatarrastream <el_chatarras@hotmail.com>
2024-11-12 00:38:53 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "mi-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
+// Instalación del Service Worker y cacheo de los archivos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercepción de las solicitudes
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Actualización del Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
